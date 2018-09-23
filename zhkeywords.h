c38bfab28f6d8ed67333d4e0839233e542fccc48
Heng Y <42090115+heng-y@users.noreply.github.com>
2018-09-23 11:36:56 -0400

    Create zhkeywords.h

---
+++ zhkeywords.h
@@ -0,0 +1,27 @@
+//ZHKEYWORDS.H - Chinese Keyword Definitions
+
+/*
+This file is part of ZHCC CHINESE C SYSTEM by Heng Ye.
+
+This is licensed under the GPL v3. See LICENSE for details.
+ */
+
+//keywords
+#ifndef ZH_ON
+#define ZH_ON 1
+#include <wchar.h>
+#define 自动 auto
+#define 中断 break
+#define 选择 case
+typedef wchar_t 字;
+typedef char 英语字;
+#define 常数 const
+#define 继续 continue
+#define 默认 default
+#define 操作 do
+typedef double 双精度;
+#define 如果 if
+#define 否则 else
+typedef int 整数;
+
+#endif
