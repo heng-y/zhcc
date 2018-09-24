4bb7235a3f4999440133a8fa5fc5524d37268595
Heng Y <42090115+heng-y@users.noreply.github.com>
2018-09-24 15:20:42 -0400

    Updated to add all 32 C keywords

--- zhkeywords.h
+++ zhkeywords.h
@@ -12,7 +12,7 @@ This is licensed under the GPL v3. See LICENSE for details.
 #include <wchar.h>
 #define 自动 auto
 #define 中断 break
-#define 选择 case
+#define 情况 case
 typedef wchar_t 字;
 typedef char 英语字;
 #define 常数 const
@@ -20,8 +20,27 @@ typedef char 英语字;
 #define 默认 default
 #define 操作 do
 typedef double 双精度;
-#define 如果 if
 #define 否则 else
+#define 列 enum
+#define 外来 extern
+typedef float 浮点;
+#define 数循环 for
+#define 跳到 goto
+#define 如果 if
 typedef int 整数;
-
+typedef long 长数;
+#define 寄存器 register
+#define 返回 return
+typedef short 短数;
+#define 正负 signed
+#define 大小 sizeof
+#define 静态 static
+#define 建构 struct
+#define 选择 switch
+#define 类型 typedef
+#define 特殊构 union
+#define 正 unsigned
+typedef void 空;
+#define 不改 volatile
+#define 循环 while
 #endif
